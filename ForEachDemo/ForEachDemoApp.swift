aa312aaabbda5ff8cd15b112988c733f861854b1
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-31 12:45:10 -0600

    Initial Commit

---
+++ ForEachDemo/ForEachDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForEachDemoApp.swift
+//  ForEachDemo
+//
+//  Created by Fred Javalera on 3/31/21.
+//
+
+import SwiftUI
+
+@main
+struct ForEachDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
