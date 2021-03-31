674151e47c6aa73c02c96d31cb94dba659c03553
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-31 13:04:38 -0600

    Finished.

--- ForEachDemo/ContentView.swift
+++ ForEachDemo/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  //MARK: - VARIABLES
+  var array = ["Pizza", "Burrito", "Sushi"]
+  
+  //MARK: - BODY
+  var body: some View {
+    //VStack
+    VStack {
+      ForEach (array, id: \.self) { recipe in
+        // This code will be repeated for each element in the array.
+        Text(recipe)
+      }
     }
+    
+    //VStack - ForEach alternative syntax with leading index displayed.
+    VStack {
+      ForEach (0..<2, id: \.self) { index in
+        // This code will be repeated for each element in the array.
+        Text(String(index) + " " + array[index])
+      }
+    }
+    
+    // ...compare to List. Similar syntax without the VStack.
+    List (array, id: \.self) { recipe in
+      // This code will be repeated for each element in the array.
+      Text(recipe)
+    }
+    
+    // ...compare to ScrollView. Combo of VStack approach and List approach above. Similar syntax.
+    ScrollView {
+      ForEach (array, id: \.self) { recipe in
+        // This code will be repeated for each element in the array.
+        Text(recipe)
+      }
+    }
+  }
 }
 
+//MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
